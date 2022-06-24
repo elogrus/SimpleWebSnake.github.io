f93e3f0c4dd0eaa082f70362e8991568ed70d1e6
elogrus <elogrus@gmail.com>
2022-06-24 23:44:33 +0500

    v3
    fix compatibility-bug with some browsers

--- js/script.js
+++ js/script.js
@@ -1,69 +1,66 @@
 const canvas = document.querySelector("#field");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#8BC34A";
-const frameDelay = 100; 
+const frameDelay = 100;
 
 let pointer = document.querySelector("#pointer");
 let gg = document.querySelector("#gg");
 let button = gg.querySelector("#reload");
-button.addEventListener("click", () => window.location.reload())
-
-
-
-
-
-
+button.addEventListener("click", () => window.location.reload());
 
 // Содержат в себе объекты вида {x,y}
-let snakePiecesList = [{x: 250, y: 250},{x: 250, y: 275}];
+let snakePiecesList = [
+    { x: 250, y: 250 },
+    { x: 250, y: 275 },
+];
 let applesList = [];
 let collectedApples = 0;
-// Направление движения головы змеи 
+// Направление движения головы змеи
 // Принимает значения: top (вверх), bottom (вниз), left (налево), rigth (направо)
-let direction = 'top';
+let direction = "top";
 let weakDirection = null;
 
 let cutSnakePromise = new Promise(() => {});
-document.addEventListener("keydown", changeDirection)
+document.addEventListener("keydown", changeDirection);
 
 function changeDirection(event) {
     switch (event) {
         case "left":
             if (direction == "right") return;
-            weakDirection = 'left';
+            weakDirection = "left";
             return;
         case "top":
             if (direction == "bottom") return;
-            weakDirection = 'top';
+            weakDirection = "top";
             return;
         case "right":
             if (direction == "left") return;
-            weakDirection = 'right';
+            weakDirection = "right";
             return;
         case "bottom":
             if (direction == "top") return;
-            weakDirection = 'bottom';
+            weakDirection = "bottom";
             return;
     }
     switch (event.keyCode) {
         case 37:
             if (direction == "right") return;
-            weakDirection = 'left';
+            weakDirection = "left";
             return;
         case 38:
             if (direction == "bottom") return;
-            weakDirection = 'top';
+            weakDirection = "top";
             return;
         case 39:
             if (direction == "left") return;
-            weakDirection = 'right';
+            weakDirection = "right";
             return;
         case 40:
             if (direction == "top") return;
-            weakDirection = 'bottom';
+            weakDirection = "bottom";
             return;
     }
-};
+}
 
 snakePiecesList.forEach((piece) => {
     if (piece == snakePiecesList[0]) {
@@ -73,32 +70,38 @@ snakePiecesList.forEach((piece) => {
     } else {
         ctx.fillRect(piece.x, piece.y, 25, 25);
     }
-})
-
+});
 
 function createApple() {
     let x = Math.random() * 500;
-    x = x - x % 25;
+    x = x - (x % 25);
     let y = Math.random() * 500;
-    y = y - y % 25;
+    y = y - (y % 25);
     let isCollusionSnake = false;
     for (piece of snakePiecesList) {
         if (piece.y == y && piece.x == x) isCollusionSnake = true;
     }
     if (isCollusionSnake) {
-        createApple()
+        createApple();
     } else {
         ctx.fillStyle = "red";
         ctx.fillRect(x, y, 25, 25);
         ctx.fillStyle = "#8BC34A";
-        applesList.push({x: x, y: y});
+        applesList.push({ x: x, y: y });
     }
 }
 
 // Удаляет яблоко из листа
 function removeApple(needToRemoveApple) {
-    let removed = applesList.splice(applesList.findIndex((apple) => apple.x == needToRemoveApple.x && apple.y == needToRemoveApple.y, 1))
-    console.log('removed: ' + removed)
+    let removed = applesList.splice(
+        applesList.findIndex(
+            (apple) =>
+                apple.x == needToRemoveApple.x &&
+                apple.y == needToRemoveApple.y,
+            1,
+        ),
+    );
+    console.log("removed: " + removed);
 }
 
 // Возвращает яблоко, которое нужно удалить. Если таких нет, то false
@@ -112,7 +115,7 @@ function needRemoveApple() {
             needToRemoveApple = apple;
         }
     }
-    
+
     if (isCollusionHead) return needToRemoveApple;
     return false;
 }
@@ -123,16 +126,20 @@ function generateApple() {
     if (needToRemoveApple) {
         addPoint(1);
         removeApple(needToRemoveApple);
-        createApple()
+        createApple();
         return true;
-    };
+    }
     return false;
 }
 
 // Если нужно обрезать змею, вернет стартовый кусок, иначе false;
 function needCutSnake() {
     for (piece of snakePiecesList) {
-        if (piece != snakePiecesList[0] && piece.x == snakePiecesList[0].x && piece.y == snakePiecesList[0].y) {
+        if (
+            piece != snakePiecesList[0] &&
+            piece.x == snakePiecesList[0].x &&
+            piece.y == snakePiecesList[0].y
+        ) {
             return piece;
         }
     }
@@ -142,10 +149,14 @@ function needCutSnake() {
 function cutSnake(startPiece) {
     let removed;
     let startPieceIndex = snakePiecesList.findIndex((findPiece) => {
-        return (findPiece != snakePiecesList[0] && findPiece.x == startPiece.x && findPiece.y == startPiece.y)
+        return (
+            findPiece != snakePiecesList[0] &&
+            findPiece.x == startPiece.x &&
+            findPiece.y == startPiece.y
+        );
     });
-    removed = snakePiecesList.splice(startPieceIndex)     
-    setPoint(getPoints() - removed.length + 1)
+    removed = snakePiecesList.splice(startPieceIndex);
+    setPoint(getPoints() - removed.length + 1);
     for (piece of removed) {
         ctx.clearRect(piece.x, piece.y, 25, 25);
     }
@@ -153,9 +164,9 @@ function cutSnake(startPiece) {
 
 function exitGame() {
     clearInterval(gameTimer);
-    gg.setAttribute("hidden", "false")
+    gg.setAttribute("hidden", "false");
     document.querySelector(".game_wrapper").style.cursor = "default";
-} 
+}
 
 function addPoint(num) {
     let pointsNow = parseInt(pointer.innerHTML);
@@ -173,14 +184,20 @@ function getPoints() {
 
 // Возвращает true, если столкнулся со стеной, иначе false
 function isWallCollusion() {
-    if (snakePiecesList[0].x < 0 || snakePiecesList[0].y < 0 || snakePiecesList[0].x > 475 || snakePiecesList[0].y > 475) return true;
+    if (
+        snakePiecesList[0].x < 0 ||
+        snakePiecesList[0].y < 0 ||
+        snakePiecesList[0].x > 475 ||
+        snakePiecesList[0].y > 475
+    )
+        return true;
     return false;
 }
 
 function drawGameFrame() {
     if (isWallCollusion()) {
         for (piece of snakePiecesList) {
-            ctx.fillStyle = "#942424"; 
+            ctx.fillStyle = "#942424";
             ctx.fillRect(piece.x, piece.y, 25, 25);
         }
         exitGame();
@@ -196,33 +213,47 @@ function drawGameFrame() {
 
     // Отрисовка змеи
     if (!eatenApple) {
-        ctx.clearRect(snakePiecesList.at(-1).x,snakePiecesList.at(-1).y,25,25);     // Убираю последний кусок змеи
-        snakePiecesList.pop()                                                      
+        ctx.clearRect(
+            snakePiecesList[snakePiecesList.length - 1].x,
+            snakePiecesList[snakePiecesList.length - 1].y,
+            25,
+            25,
+        ); // Убираю последний кусок змеи
+        snakePiecesList.pop();
     }
 
-    switch (direction) {                                                       // Рисую голову змеи взависимости от направления движения
-        case 'top':
-            snakePiecesList.unshift({x: snakePiecesList[0].x, y: snakePiecesList[0].y - 25})
+    switch (
+        direction // Рисую голову змеи взависимости от направления движения
+    ) {
+        case "top":
+            snakePiecesList.unshift({
+                x: snakePiecesList[0].x,
+                y: snakePiecesList[0].y - 25,
+            });
             break;
-        case 'bottom':
-            snakePiecesList.unshift({x: snakePiecesList[0].x, y: snakePiecesList[0].y + 25})
+        case "bottom":
+            snakePiecesList.unshift({
+                x: snakePiecesList[0].x,
+                y: snakePiecesList[0].y + 25,
+            });
             break;
-        case 'right':
-            snakePiecesList.unshift({x: snakePiecesList[0].x + 25, y: snakePiecesList[0].y})
+        case "right":
+            snakePiecesList.unshift({
+                x: snakePiecesList[0].x + 25,
+                y: snakePiecesList[0].y,
+            });
             break;
-        case 'left':
-            snakePiecesList.unshift({x: snakePiecesList[0].x - 25, y: snakePiecesList[0].y})
+        case "left":
+            snakePiecesList.unshift({
+                x: snakePiecesList[0].x - 25,
+                y: snakePiecesList[0].y,
+            });
             break;
     }
-    ctx.fillStyle = "#689F38"; 
+    ctx.fillStyle = "#689F38";
     ctx.fillRect(snakePiecesList[0].x, snakePiecesList[0].y, 25, 25);
-    ctx.fillStyle = "#8BC34A"; // Перекрашиваю часть перед новой головой (бывшую голову) 
+    ctx.fillStyle = "#8BC34A"; // Перекрашиваю часть перед новой головой (бывшую голову)
     ctx.fillRect(snakePiecesList[1].x, snakePiecesList[1].y, 25, 25);
-
-  
-
-    
-    
 }
 //--------------------------------------//
 
@@ -235,18 +266,18 @@ let i = 3;
 
 function countTimer() {
     if (i == 0) {
-        timerWrapper.setAttribute("hidden", "true")
+        timerWrapper.setAttribute("hidden", "true");
         clearInterval(preGameTimer);
     }
 
     timerNumber.style.transition = "none";
     timerNumber.style.transform = "scale(0.2)";
-    
+
     timerNumber.getBoundingClientRect();
 
     timerNumber.style.transition = "transform 1s";
     timerNumber.style.transform = "scale(1)";
-    
+
     timerNumber.innerHTML = String(i);
     i--;
 }
@@ -262,8 +293,3 @@ setTimeout(() => {
         drawGameFrame();
     }, 100);
 }, 3000);
-
-
-
-
-
